08aaab45f04dec2319c6957343db3d0ce1f7429d
Asma <asma.shaheed@aol.com>
2021-03-03 21:09:57 +0000

    Lots of lines of code, I deleted min/max

--- main.cs
+++ main.cs
@@ -19,37 +19,29 @@ class MainClass {
        len = EvenNum.Length;
       Console.WriteLine("Length = " + len);
 
-      
-      for(int pos=0; pos < len; pos++)
-      {
-        Console.WriteLine(EvenNum[pos]);
-      }
-
-      Console.WriteLine("Asending");
+    
+      //Desending
+      Console.WriteLine("Desending order of odd numbers:");
       Array.Sort(OddNum);
-      for(int pos=0; pos > len; pos++)
+      for(int pos=0; pos < len; pos++)
       {
         Console.WriteLine(OddNum[pos]);
       }
       
-      Console.WriteLine("Reverse");
+      //asending 
+      Console.WriteLine("Asending order of even numbers: ");
       Array.Reverse(EvenNum);
       for(int pos=0; pos < len; pos++)
       {
         Console.WriteLine(EvenNum[pos]);
       }
-    
-        int min = OddNum.Min();
-        Console.WriteLine("Min value = " + min);
-
-        int max = EvenNum.Max();
-        Console.WriteLine("Max value = " + max);
+  
         //find sum of even and odd numbers and display
         int sum1 = EvenNum.Sum();
-        Console.WriteLine("Sum = " + sum1);
+        Console.WriteLine("Sum of even numbers = " + sum1);
 
         int sum2 = OddNum.Sum();
-        Console.WriteLine("Sum = " + sum2);
+        Console.WriteLine("Sum of odd numbers = " + sum2);
         
   }
 }
